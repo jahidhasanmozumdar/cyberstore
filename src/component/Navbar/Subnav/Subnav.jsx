e161733ffb6f440aaf586535442cf4b2fae4b652
jahidhasanmozumdar <jahidhasanmozumdar9@gmail.com>
2023-08-14 07:58:23 +0600

    divider add on navbar

--- src/component/Navbar/Subnav/Subnav.jsx
+++ src/component/Navbar/Subnav/Subnav.jsx
@@ -4,7 +4,7 @@ import { GiSelfLove } from "react-icons/Gi";
 
 function Subnav() {
   return (
-    <div className="sm:invisible md:visible lg:visible min-h-[41px] bg-[#253237] flex justify-center items-center gap-[980px] ">
+    <div className="min-h-[41px] bg-[#253237] justify-around items-center  hidden sm:flex">
       <div className="flex justify-center items-center min-h-[21px]">
         <h3 className="text-[13px] text-[#FFFFFF]">
           Welcome you to Ecolife store!
@@ -12,20 +12,20 @@ function Subnav() {
       </div>
       <div className="min-h-[21px]">
         <div>
-          <ul className="flex flex-row justify-center items-center gap-[20px] text-[14px]">
-            <li className="text-[#FFFFFF] hover:text-[#36AA82] flex justify-center items-center gap-[5px]">
+          <ul className="flex flex-row justify-center items-center gap-[20px] text-[14px] divide-x-[1px] divide-white">
+            <li className="text-[#FFFFFF] hover:text-[#36AA82] flex justify-center items-center gap-[5px] px-4 py-1">
               <BiUser></BiUser>
               sign in
             </li>
-            <li className="text-[#FFFFFF] hover:text-[#36AA82] flex justify-center items-center gap-[5px]">
+            <li className="text-[#FFFFFF] hover:text-[#36AA82] flex justify-center items-center gap-[5px] px-4 py-1">
               <HiArrowsRightLeft></HiArrowsRightLeft>
               Compare
             </li>
-            <li className="text-[#FFFFFF] hover:text-[#36AA82] flex justify-center items-center gap-[5px]">
+            <li className="text-[#FFFFFF] hover:text-[#36AA82] flex justify-center items-center gap-[5px] px-4 py-1">
               <GiSelfLove></GiSelfLove>
               Wishlist
             </li>
-            <li className="text-[#FFFFFF] hover:text-[#36AA82]  ">
+            <li className="text-[#FFFFFF] hover:text-[#36AA82]  px-4 py-1">
               <div>
                 <select className="bg-[#253237] focus:outline-none hover:text-[#36AA82]">
                   <option value="EUR">EUR</option>
