2d7a0c35a114013d20781c63c410e2a8640a1041
jahidhasanmozumdar <jahidhasanmozumdar9@gmail.com>
2023-08-14 06:20:44 +0600

    ecommarce project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Subnav from "./component/Navbar/Subnav/Subnav";
+
+function App() {
+  return (
+    <>
+      <Subnav></Subnav>
+    </>
+  );
+}
+
+export default App;
